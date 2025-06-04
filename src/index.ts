63a46a0077ea16481b606bd8d363ee9c0ff15b73
Ulugbek Tojikulov <cookie.pechehbkov@mail.ru>
2025-06-04 14:04:24 +0500

    logic completed/ need to write a function to control the speed of typing and selecting the options of the test

--- src/index.ts
+++ src/index.ts
@@ -2,24 +2,60 @@ import puppeteer from "puppeteer";
 
 const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
-async function testBot() {
+// async function testBot() {
+//   const browser = await puppeteer.launch({ headless: false });
+//   const selector = 'input[name="search_query"]';
+//   try {
+//     const page = await browser.newPage();
+//     await page.goto("https://www.youtube.com", {
+//       waitUntil: "domcontentloaded",
+//     });
+//     await page.waitForSelector(selector);
+//     await page.click("input.yt-searchbox-input");
+//     await page.type(selector, "whosdis?");
+//     await page.keyboard.press("Enter");
+//     await delay(3000);
+//   } catch (err) {
+//     console.log("error occured", err);
+//   } finally {
+//     await browser.close();
+//   }
+// }
+
+// testBot();
+
+async function main() {
   const browser = await puppeteer.launch({ headless: false });
-  const selector = 'input[name="search_query"]';
+  // const wordsWrapper = "div#wordsWrapper";
+  const page = await browser.newPage();
+  const cookiesModal = "dialog#cookiesModal";
   try {
-    const page = await browser.newPage();
-    await page.goto("https://www.youtube.com", {
+    await page.goto("https://www.monkeytype.com", {
       waitUntil: "domcontentloaded",
     });
-    await page.waitForSelector(selector);
-    await page.click("input.yt-searchbox-input");
-    await page.type(selector, "whosdis?");
-    await page.keyboard.press("Enter");
-    await delay(3000);
+    if (cookiesModal) {
+      await page.click("button.rejectAll");
+    }
+
+    // focusing the typing area
+    await page.waitForSelector(".word");
+
+    // extract the word list
+    const words = await page.$$eval(".word", (els) =>
+      els.map((el) => el.textContent?.trim() || ""),
+    );
+
+    console.log(words);
+    for (const word of words) {
+      await page.keyboard.type(word + " ");
+    }
+
+    await delay(5000);
   } catch (err) {
     console.log("error occured", err);
   } finally {
-    await browser.close();
+    // await browser.close()
   }
 }
 
-testBot();
+main();
